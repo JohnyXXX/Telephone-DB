cf77ff8e14500c6d798b235d008c62fd91d55cf0
JohnyXXX <johnyeev@gmail.com>
2020-04-29 19:02:12 +0500

    Добавил конструктор и представление для класса Person

--- main.py
+++ main.py
@@ -2,12 +2,22 @@ class TelephoneDB:
     pass
 
 
-class Human:
-    pass
+class Person:
+    def __init__(self, fullname, address, telephone):
+        self.fullname = fullname
+        self.address = address
+        self.telephone = telephone
+
+    def __repr__(self):
+        return f'ФИО: {self.fullname}; Адрес: {self.address}; Телефон: {self.telephone}'
 
 
 if __name__ == '__main__':
-    a_human = Human.add('Иванов Иван Иванович', 'Ленина, 45, 23', '+70000000001')
-    b_human = Human.add('Прекрасная Елена Павловна', 'Кутузова, 6, 123', '+70000000002')
-    c_human = Human.add('Вольная Ольга Александровна', 'Парковая, 50, 82', '+70000000003')
-    d_human = Human.add('Гордый Михаил Львович', 'Центральный, 142, 73', '+70000000004')
+    a_person = Person('Иванов Иван Иванович', 'Ленина, 45, 23', '+70000000001')
+    b_person = Person('Прекрасная Елена Павловна', 'Кутузова, 6, 123', '+70000000002')
+    c_person = Person('Вольная Ольга Александровна', 'Парковая, 50, 82', '+70000000003')
+    d_person = Person('Гордый Михаил Львович', 'Центральный, 142, 73', '+70000000004')
+    print(a_person)
+    print(b_person)
+    print(c_person)
+    print(d_person)
