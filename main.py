3d3655b8ec8b9416b5f83b877dea069abe384174
JohnyXXX <johnyeev@gmail.com>
2020-04-30 17:52:25 +0500

    Добавил проверку на существующую запись в базе

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
+class TelephoneExist(Exception):
+    pass
+
+
 class TelephoneDB(list):
-    db = []
+    """
+    Класс БД на основе списка. Должен добавлять,
+    искать, изменять и удалять из БД.
+    """
+
+    __db = []
+
+    def __person_exist_check(self, person):
+        for i in self.__db:
+            if person.telephone == i.telephone:
+                return True
 
     def append(self, person):
-        self.db.append(person)
+        if self.__person_exist_check(person) is True:
+            raise TelephoneExist
+        else:
+            self.__db.append(person)
+        print(self.__db)
 
     def find(self, request):
-        for row in self.db:
+        for row in self.__db:
             if (request in row.fullname) or (request in row.address) or (request in row.telephone):
                 return row
 
 
 class Person:
+    """
+    Конструктор для "Персоны" и представление для печати.
+    """
+
     def __init__(self, fullname, address, telephone):
         self.fullname = fullname
         self.address = address
@@ -25,16 +47,19 @@ if __name__ == '__main__':
     b_person = Person('Прекрасная Елена Павловна', 'Кутузова, 6, 123', '+70000000002')
     c_person = Person('Вольная Ольга Александровна', 'Парковая, 50, 82', '+70000000003')
     d_person = Person('Гордый Михаил Львович', 'Центральный, 142, 73', '+70000000004')
-    print(a_person)
-    print(b_person)
-    print(c_person)
-    print(d_person)
+    e_person = Person('', '', '')
+    f_person = Person('Гордый Михаил Львович', 'Центральный, 142, 73', '+70000000004')
+    # print(a_person)
+    # print(b_person)
+    # print(c_person)
+    # print(d_person)
     db = TelephoneDB()
     db.append(a_person)
     db.append(b_person)
     db.append(c_person)
     db.append(d_person)
-    print(len(db.db), db.db)
+    db.append(e_person)
+    db.append(f_person)
     print(db.find('Ольга'))
     print(db.find('Иванович'))
     print(db.find('Центральный'))
